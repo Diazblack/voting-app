d20b79a11fe7f41821b18406e63db7b0798025fc
Cesar Jolibois <diazcd85@gmail.com>
2025-05-20 18:12:45 -0400

    Added vote column to user table, updated models

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,3 @@
 class Candidate < ApplicationRecord
+  has_many :voters, class_name: 'User', foreign_key: 'vote_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-
+  belongs_to :vote, class_name: "Candidate", optional: true
   validates :email, presence: true, uniqueness: true 
 end
